914940425edeec52652270822f00028f1fdefa19
Rado | Nezo96 <racradoslav96@gmail.com>
2023-08-25 19:14:40 +0200

    Added validations and errors session

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -30,6 +32,19 @@ Route::get('/tasks/{id}', function($id) {
     return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
 
-Route::post('/tasks', function () {
-    dd('we have reached the store method');
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task();
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
