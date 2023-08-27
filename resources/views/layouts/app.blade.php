afba19d1c5eca18358ae6e44731fb12bb741f411
Rado | Nezo96 <racradoslav96@gmail.com>
2023-08-27 23:50:12 +0200

    Refactored forms with "Subviews"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <title>Document</title>
     @yield('styles')
 </head>
+
 <body>
     <h1>@yield('title')
     </h1>
@@ -17,4 +19,5 @@
         @yield('content')
     </div>
 </body>
+
 </html>
