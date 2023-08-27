94623894e3c8ae42493622045b300107774d5423
Rado | Nezo96 <racradoslav96@gmail.com>
2023-08-27 23:55:50 +0200

    Added pagination

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::all()
+        'tasks' => Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
