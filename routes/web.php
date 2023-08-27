3bb658b204a849f2a709b9ecb236fb4b04480743
Rado | Nezo96 <racradoslav96@gmail.com>
2023-08-27 23:08:29 +0200

    Added edit page and update function to update data

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,27 @@ Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
 
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', ['task' => \App\Models\Task::findOrFail($id)]);
+})->name('tasks.edit');
+
+Route::put('/tasks/{id}', function (Request $request, $id) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task updated successfully!');
+})->name('tasks.update');
+
 Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
         'title' => 'required|max:255',
